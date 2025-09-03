06ee2324b231e714fac1721c1df2a67737b34143
Lucas Dev <lucasemmanuelrodrigues737@gmail.com>
2025-09-03 15:14:42 -0300

    FEAT: Create the scr/components/title dir

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Title from './src/components/title';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!!!</Text>
-      <StatusBar style="auto" />
+      <Title />
     </View>
   );
 }
