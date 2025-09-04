c961411fb63ccc86978e0731a12fd91a529b227b
Lucas Dev <lucasemmanuelrodrigues737@gmail.com>
2025-09-04 11:07:27 -0300

    FEAT: Add the share button

--- src/components/form/result-imc/index.js
+++ src/components/form/result-imc/index.js
@@ -1,10 +1,25 @@
 import React from "react"
-import { View, Text} from "react-native"
+import { View, Text, TouchableOpacity, Share} from "react-native"
 import styles from "./style";
 
 export default function ResultImc(props){
+    const onShare = async () => {
+        const result = await Share.share({
+            message: "Meu imc hoje é: " + props.resultImc
+        });
+    }
+
     return(
         <View style={styles.resultImc}>
+            <View style={styles.sharedBox}>
+                {
+                props.resultImc != null ?
+                <TouchableOpacity style={styles.shareButton} onPress={onShare}>
+                    <Text style={styles.sharedText}>Compartilhar</Text>
+                </TouchableOpacity>
+                : <View />
+                }
+            </View>
             <Text style={styles.infomartion}>{props.messageResultImc}</Text>
             <Text style={styles.numberImc}>{props.resultImc}</Text>
         </View>
--- src/components/form/result-imc/style.js
+++ src/components/form/result-imc/style.js
@@ -8,7 +8,7 @@ const styles = StyleSheet.create({
         borderRadius: 50,
         alignItems: "center",
     },
-    numberImc:{
+    numberImc: {
         fontSize: 48,
         color: "#FF0043",
         width: "100%",
@@ -18,6 +18,23 @@ const styles = StyleSheet.create({
         fontSize: 18,
         color: "#FF0043",
         fontWeight: "bold"
+    },
+    sharedBox: {
+        width: "100%",
+        alignItems: "center",
+        marginBottom: 10
+    },
+    sharedText: {
+        fontSize: 15,
+        color: "#ffffff",
+        fontWeight: "bold",
+        paddingHorizontal: 30
+    },
+    shareButton: {
+        backgroundColor: "#1877f2",
+        borderRadius: 50,
+        paddingTop: 5,
+        paddingBottom: 5,
     }
 })
 
